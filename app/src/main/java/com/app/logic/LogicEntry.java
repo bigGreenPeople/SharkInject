4a7b51cf45b64c5649cf31cd0ddc082f0049672d
fujie <1243596620@qq.com>
2021-05-23 18:20:07 +0800

    gradle自动部署文件编写

--- app/src/main/java/com/app/logic/LogicEntry.java
+++ app/src/main/java/com/app/logic/LogicEntry.java
@@ -8,7 +8,7 @@ import com.app.service.AbstractEntry;
 public class LogicEntry extends AbstractEntry {
     @Override
     public void activityOnCreate(Activity activity) {
-        Log.i(TAG, "activityOnCreate: " + activity.getClass().getName());
+        Log.i(TAG, " activityOnCreate: " + activity.getClass().getName());
     }
 
     @Override
